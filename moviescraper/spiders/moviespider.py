8313fa2217730554e460a4430c5ddadaeb09be8a
t7bo <tibo7w@gmail.com>
2024-03-15 20:45:28 +0100

    new info scrapped

--- moviescraper/spiders/moviespider.py
+++ moviescraper/spiders/moviespider.py
@@ -23,4 +23,15 @@ class MoviespiderSpider(scrapy.Spider):
             'year' : response.xpath('//section/div[2]/div[1]/ul/li[1]/a/text()').get(),
             'public' : response.xpath('//section/div[2]/div[1]/ul/li[2]/a/text()').get(),
             'screening' : response.xpath('//section/div[2]/div[1]/ul/li[3]/text()').get(),
+            'mark' : response.xpath('//section/div[2]/div[2]/div/div[1]/a/span/div/div[2]/div[1]/span/text()').get(),
+            'marks_nb' : response.xpath('//section/div[2]/div[2]/div/div[1]/a/span/div/div[2]/div[3]/text()').get(),
+            "popularity" : response.xpath('//div[@data-testid="hero-rating-bar__popularity__score"]/text()').get(),
+            'category' : response.xpath('//section/div[3]/div[2]/div[1]/section/div[1]/div[2]/a/span/text()').get(),
+            'synopsis' : response.xpath('//span[@data-testid="plot-xl"]/text()').get(),
+            'director' : response.xpath('//section/div[3]/div[2]/div[1]/section/div[2]/div/ul/li[1]/div/ul/li/a/text()').get(),
+            # 'casting' : list(response.xpath('//section/div[3]/div[2]/div[1]/section/div[2]/div/ul/li[3]/div/ul//following-sibling::li[0]//a/text()').get(),
+            #                  response.xpath('//section/div[3]/div[2]/div[1]/section/div[2]/div/ul/li[3]/div/ul//following-sibling::li[1]//a/text()').get(),
+            #                  response.xpath('//section/div[3]/div[2]/div[1]/section/div[3]/div/ul/li[3]/div/ul//following-sibling::li[2]//a/text()').get()),
+            "budget" : response.xpath('//main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[1]/div/ul/li/span/text()').get(),
+            'worldwide_boxoffice' : response.xpath('//main/div/section[1]/div/section/div/div[1]/section[12]/div[2]/ul/li[4]/div/ul/li/span/text()').get()
         }
