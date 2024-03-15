60c240bb1365045aeed1926f4783a1b6914680e2
t7bo <tibo7w@gmail.com>
2024-03-15 16:43:47 +0100

    scrapy genspider moviespider https://www.imdb.com/chart/top/

---
+++ moviescraper/spiders/moviespider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class MoviespiderSpider(scrapy.Spider):
+    name = "moviespider"
+    allowed_domains = ["www.imdb.com"]
+    start_urls = ["https://www.imdb.com/chart/top/"]
+
+    def parse(self, response):
+        pass
